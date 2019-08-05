07cb72b92637630e42da991679c85a4e9b8b665b
SCunn <43576829+SCunn@users.noreply.github.com>
2019-08-05 15:51:21 +0100

    Update ImageGallery.js

--- ImageGallery.js
+++ ImageGallery.js
@@ -1,3 +1,24 @@
+//  ------------ navbar menu mobile -----------
+$(document).ready(function(){
+	$("#flip").click(function(){
+		$("#navMenuDrop").slideToggle(1100);
+	});
+});
+
+$(document).ready(function(){
+	$(document).scroll(function(){
+		$("#navMenuDrop").fadeOut(2500);
+		
+	});
+});
+
+// ------------- Sound Effects click ----------
+var klIck = $("#klick")[0];
+$("#myTopNav a, #flip, #myLink").click(function() {
+		klIck.play();
+	});
+
+// ----------- Image Gallery ----------------
 var imageCount = 1;
 var total = 10;
 
@@ -37,4 +58,21 @@ var time = window.setInterval(function photoA() {    // just addign the sunction
     image.src = "imagegallery/image" + imageCount + ".png";
 }, 3000);
 
+
+
+// -------------- Tabbed Menu ----------------
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+document.getElementById("myLink").click();
 
